3b9bbf1d9357d7cc87648a9a73f4811c7b3a5b15
appanacca <nico.lumi@gmail.com>
2015-07-08 15:26:48 +0200

    introduced blasius inizialization

--- pyLisa.py
+++ pyLisa.py
@@ -17,6 +17,7 @@ import scipy.interpolate as intp
 
 import scipy.io
 
+import blasius as bl
 
 
 
@@ -39,8 +40,8 @@ class fluid(object):
 		self.U_data=in_txt[:,1]
 		self.dU_data=in_txt[:,2]
 		self.ddU_data=in_txt[:,3]
-		self.aCD_data=np.zeros(len(self.y_data))
-		#self.aCD_data=in_txt[:,4]
+		#self.aCD_data=np.zeros(len(self.y_data))
+		self.aCD_data=in_txt[:,4]
 		#self.daCD_data=in_txt[:,5]
 		self.lc=0.16898  #lc*=0.22*(h-z1) / h 
 
@@ -52,12 +53,17 @@ class fluid(object):
 		self.dU=dUpoiseuille(self.y)
 		self.ddU=ddUpoiseuille
 
+
+	def set_blasisus(self,y_gl):
+		self.U, self.dU, self.ddU = bl.blasius(y_gl) 
+
+
 	def plot_velocity(self):
 		fig, ay = plt.subplots(figsize=(10,10), dpi=50)
-		lines = ay.plot(self.U,self.y,'b',self.dU,self.y,'g',self.ddU,self.y,'r',lw=2)
+		lines = ay.plot(self.U,self.y,'b',self.dU,self.y,'g',self.ddU,self.y,'r',self.aCD,self.y,'m',lw=2)
 		ay.set_ylabel(r'$y$',fontsize=32)
-		lgd=ay.legend((lines),(r'$U$',r'$\delta U$',r'$\delta^2 U$'),loc = 3,ncol=3, bbox_to_anchor = (0,1),fontsize=32)
-		#ax.set_ylim([0.3,0.9])
+		lgd=ay.legend((lines),(r'$U$',r'$\delta U$',r'$\delta^2 U$',r'$a^* \dot C_D$'),loc = 3,ncol=3, bbox_to_anchor = (0,1),fontsize=32)
+		ay.set_ylim([0,5])
 		#ax.set_xlim([np.min(time[2*T:3*T]),np.max(time[2*T:3*T])])
 		ay.grid()                    
 		#plt.tight_layout()
@@ -79,14 +85,14 @@ class fluid(object):
          
   
 	def build_operator(self):
-	 I=np.identity(self.N)
-         self.A= np.dot(np.diag(self.alpha*self.U),(self.D[1]-I*self.alpha**2)) -np.diag(self.alpha*self.ddU) +((1/self.Re)*(self.D[3] -(2*self.alpha**2)*self.D[1] +(self.alpha**4)*I ))*(0+1j)
-         self.B=(self.D[1]-I*self.alpha**2)
+		 I=np.identity(self.N)
+		 self.A= np.dot(np.diag(self.alpha*self.U),(self.D[1]-I*self.alpha**2)) -np.diag(self.alpha*self.ddU) +((1/self.Re)*(self.D[3] -(2*self.alpha**2)*self.D[1] +(self.alpha**4)*I ))*(0+1j)
+		 self.B=(self.D[1]-I*self.alpha**2)
 
 
 	def set_perturbation(self,a,re):
-         self.alpha=a
-         self.Re=re 
+		 self.alpha=a
+		 self.Re=re 
 
 
 	def BC1(self):
@@ -127,56 +133,66 @@ class fluid(object):
          
          
 	def solve_eig(self):
-         self.eigv, self.eigf  = lin.eig(self.A,self.B) #, left=True, right=True)
+		 self.eigv, self.eigf  = lin.eig(self.A,self.B) #, left=True, right=True)
 
-	 #remove the infinite and nan eigenvectors, and their eigenfunctions
-	 """selector=np.isfinite(self.eigv)
-	 self.eigv=self.eigv[selector]
-	 self.eigf=self.eigf[:,selector]"""
+		 #remove the infinite and nan eigenvectors, and their eigenfunctions
+		 """selector=np.isfinite(self.eigv)
+		 self.eigv=self.eigv[selector]
+		 self.eigf=self.eigf[:,selector]"""
 
-	 self.eigv_re=np.real(self.eigv)
-	 self.eigv_im=np.imag(self.eigv)
+		 self.eigv_re=np.real(self.eigv)
+		 self.eigv_im=np.imag(self.eigv)
 		
 	
 	def plot_spectrum(self):
-		fig, ay = plt.subplots(figsize=(10,10), dpi=50)
-		lines = ay.plot(self.eigv_re,self.eigv_im,'b*',lw=2)
-		ay.set_ylabel(r'$\omega_i$',fontsize=32)
-		ay.set_xlabel(r'$\omega_r$',fontsize=32)
-		#lgd=ay.legend((lines),(r'$U$',r'$\delta U$',r'$\delta^2 U$'),loc = 3,ncol=3, bbox_to_anchor = (0,1),fontsize=32)
-		ay.set_ylim([-1,0.1])
-		ay.set_xlim([0, 1])
-		ay.grid()                                         
-		#plt.tight_layout()
-		fig.savefig('RESULTS'+'spectrum_couette.png', bbox_inches='tight',dpi=50)     
-		#plt.show(lines)	
-		sel_eig=plt.ginput(2)
-		
-		omega_r_picked=(sel_eig[0][0] +sel_eig[1][0])/2
-		omega_i_picked=(sel_eig[0][1] +sel_eig[1][1])/2
-		
-		omega_picked=omega_r_picked*(1+0j)+ omega_i_picked*(0+1j)
-		n=np.argmin(np.abs(self.eigv -omega_picked))
+		for i in np.arange(10):
+			fig, ay = plt.subplots(figsize=(10,10), dpi=50)
+			lines = ay.plot(self.eigv_re,self.eigv_im,'b*',lw=2)
+			ay.set_ylabel(r'$\omega_i$',fontsize=32)
+			ay.set_xlabel(r'$\omega_r$',fontsize=32)
+			#lgd=ay.legend((lines),(r'$U$',r'$\delta U$',r'$\delta^2 U$'),loc = 3,ncol=3, bbox_to_anchor = (0,1),fontsize=32)
+			ay.set_ylim([-1,0.1])
+			ay.set_xlim([0, 1])
+			ay.grid()                                         
+			#plt.tight_layout()
+			fig.savefig('RESULTS'+'spectrum_couette.png', bbox_inches='tight',dpi=50)     
+			#plt.show(lines)	
 
-		eigfun_picked=self.eigf[:,n]#*(-0.13 -0.99j)
 		
-		print omega_picked, lin.norm(eigfun_picked)
-
-		fig2, ay2 = plt.subplots(figsize=(10,10), dpi=50)
-		lines2 = ay2.plot(np.real(eigfun_picked),self.y,'r',np.imag(eigfun_picked),self.y,'g',np.sqrt(eigfun_picked*np.conjugate(eigfun_picked)),self.y,'m',lw=2)
-		ay2.set_ylabel(r'$y$',fontsize=32)
-		ay2.set_xlabel(r"$v^'$",fontsize=32)	
-		lgd=ay2.legend((lines2),(r'$Re$',r'$Im$',r'$Mod$'),loc = 3,ncol=3, bbox_to_anchor = (0,1),fontsize=32)
-
-		ay2.set_ylim([0,5])
-		ay2.set_xlim([-1, 1])
-		ay2.grid()                                         
-		#plt.tight_layout()
-		fig2.savefig('RESULTS'+'eig_fun.png', bbox_inches='tight',dpi=50)   
-
-		#print omega_picked
+			sel_eig=plt.ginput(2)
 		
-		plt.show(lines)	
+			omega_r_picked=(sel_eig[0][0] +sel_eig[1][0])/2
+			omega_i_picked=(sel_eig[0][1] +sel_eig[1][1])/2
+		
+			omega_picked=omega_r_picked*(1+0j)+ omega_i_picked*(0+1j)
+			n=np.argmin(np.abs(self.eigv -omega_picked))
+
+			eigfun_picked=self.eigf[:,n]#*(-0.13 -0.99j)
+		
+			print omega_picked, lin.norm(eigfun_picked)
+
+			v=eigfun_picked
+			u=np.dot((v/self.alpha),  self.D[0]) *(0+1j)
+
+			fig2, (ay2,ay3) = plt.subplots(1,2)#, dpi=50)
+			lines2 = ay2.plot(np.real(u),self.y,'r',np.imag(u),self.y,'g',np.sqrt(u*np.conjugate(u)),self.y,'m',lw=2)
+			ay2.set_ylabel(r'$y$',fontsize=32)
+			ay2.set_xlabel(r"$u^'$",fontsize=32)	
+			#lgd=ay2.legend((lines2),(r'$Re$',r'$Im$',r'$Mod$'),loc = 3,ncol=3, bbox_to_anchor = (0,1),fontsize=32)
+			ay2.set_ylim([0,5])
+			#ay2.set_xlim([-1, 1])
+			ay2.grid()  
+
+
+			lines3 = ay3.plot(np.real(v),self.y,'r',np.imag(v),self.y,'g',np.sqrt(v*np.conjugate(v)),self.y,'m',lw=2)
+			ay3.set_ylabel(r'$y$',fontsize=32)
+			ay3.set_xlabel(r"$v^'$",fontsize=32)	
+			#lgd=ay3.legend((lines3),(r'$Re$',r'$Im$',r'$Mod$'),loc = 3,ncol=3, bbox_to_anchor = (0,1),fontsize=32)
+			ay3.set_ylim([0,5])
+			#ay3.set_xlim([-1, 1])
+			ay3.grid()  
+			
+			plt.show(lines)
 		
 	
 		
@@ -224,13 +240,13 @@ class fluid(object):
 		AA3=self.D[0]
 
 		AB1=i*self.alpha*I
-		AB2=i*self.alpha*np.diag(self.U) -delta/self.Re +np.diag(self.aCD*self.U) #-(2*self.lc**2 )*(np.dot(np.diag(self.dU),self.D[1]) + np.dot( self.D[0] ,np.diag(self.ddU))     )  
+		AB2=i*self.alpha*np.diag(self.U)  +np.diag(self.aCD*self.U) #-delta/self.Re -(2*self.lc**2 )*(np.dot(np.diag(self.dU),self.D[1]) + np.dot( self.D[0] ,np.diag(self.ddU))     )  
 
 		AB3=np.diag(self.dU)
 
 		AC1=self.D[0]
 		AC2=np.zeros((self.N,self.N))
-		AC3=+ i*self.alpha*np.diag(self.U)  -delta/self.Re 
+		AC3=+ i*self.alpha*np.diag(self.U) # -delta/self.Re 
 
 		BA1=BA2=BA3=BB1=BB3=BC1=BC2=np.zeros((self.N,self.N))
 		BB2=BC3=i*self.alpha*I
@@ -263,6 +279,22 @@ class fluid(object):
 		
 		#print self.A, self.B
 
+
+	def BC_LNS_2(self):
+		idx_bc=np.array([self.N,2*self.N,3*self.N -1]) #index of the 
+		
+		self.A[idx_bc,:]=np.zeros(3*self.N)
+		self.B[idx_bc,:]=np.zeros(3*self.N)
+		
+		self.A[self.N,self.N]=1
+		#self.A[2*self.N -1,2*self.N -1]=1
+		
+		self.A[2*self.N ,2*self.N ]=1
+		self.A[3*self.N -1,3*self.N -1]=1
+		
+		#print self.A, self.B
+
+
 	def interpolate(self):
 		f_U=intp.interp1d(self.y_data,self.U_data)
 		idx=np.where(self.y<self.y_data[-1])
@@ -282,99 +314,97 @@ class fluid(object):
 		
 
 	def plot_LNS_eigspectrum(self):    
-		fig, ay = plt.subplots(figsize=(10,10), dpi=50)
-		lines = ay.plot(self.eigv_re,self.eigv_im,'b*',lw=2)
-		ay.set_ylabel(r'$\omega_i$',fontsize=32)
-		ay.set_xlabel(r'$\omega_r$',fontsize=32)
-		#lgd=ay.legend((lines),(r'$U$',r'$\delta U$',r'$\delta^2 U$'),loc = 3,ncol=3, bbox_to_anchor = (0,1),fontsize=32)
-		ay.set_ylim([-5,0.1])
-		ay.set_xlim([0, 1.7])
-		ay.grid()                                         
-		#plt.tight_layout()
-		fig.savefig('RESULTS'+'spectrum_bla.png', bbox_inches='tight',dpi=50)     
-		#plt.show(lines)	
-		sel_eig=plt.ginput(2)
+		for i in np.arange(10):
+			fig, ay = plt.subplots(figsize=(10,10), dpi=50)
+			lines = ay.plot(self.eigv_re,self.eigv_im,'b*',lw=2)
+			ay.set_ylabel(r'$\omega_i$',fontsize=32)
+			ay.set_xlabel(r'$\omega_r$',fontsize=32)
+			#lgd=ay.legend((lines),(r'$U$',r'$\delta U$',r'$\delta^2 U$'),loc = 3,ncol=3, bbox_to_anchor = (0,1),fontsize=32)
+			ay.set_ylim([-1,0.1])
+			ay.set_xlim([0, 1.8])
+			ay.grid()                                         
+			#plt.tight_layout()
+			fig.savefig('RESULTS'+'spectrum_bla.png', bbox_inches='tight',dpi=50)     
+			#plt.show(lines)	
+				
+			sel_eig=plt.ginput(2)
 		
-		omega_r_picked=(sel_eig[0][0] +sel_eig[1][0])/2
-		omega_i_picked=(sel_eig[0][1] +sel_eig[1][1])/2
+			omega_r_picked=(sel_eig[0][0] +sel_eig[1][0])/2
+			omega_i_picked=(sel_eig[0][1] +sel_eig[1][1])/2
 		
-		omega_picked=omega_r_picked*(1+0j)+ omega_i_picked*(0+1j)
-		n=np.argmin(np.abs(self.eigv -omega_picked))
+			omega_picked=omega_r_picked*(1+0j)+ omega_i_picked*(0+1j)
+			n=np.argmin(np.abs(self.eigv -omega_picked))
 		
-		eigfun_picked=self.eigf[:,n]
+			eigfun_picked=self.eigf[:,n]
 		
-		print omega_picked, lin.norm(eigfun_picked), len(eigfun_picked)
+			print omega_picked, lin.norm(eigfun_picked), len(eigfun_picked)
 
-		p=eigfun_picked[0:self.N]
-		u=eigfun_picked[self.N:2*self.N]
-		v=eigfun_picked[2*self.N:3*self.N]
-
-		
-		fig2, (ay1,ay2,ay3) = plt.subplots(1,3)#, dpi=50)
-		lines1 = ay1.plot(np.real(p),self.y,'r',np.imag(p),self.y,'g',np.sqrt(p*np.conjugate(p)),self.y,'m',lw=2)
-		ay1.set_ylabel(r'$y$',fontsize=32)
-		ay1.set_xlabel(r"$p^'$",fontsize=32)	
-		lgd=ay1.legend((lines1),(r'$Re$',r'$Im$',r'$Mod$'),loc = 3,ncol=3, bbox_to_anchor = (0,1),fontsize=32)
-		ay1.set_ylim([0,5])
-		ay1.set_xlim([-1, 1])
-		ay1.grid()                                         
-		
-		lines2 = ay2.plot(np.real(u),self.y,'r',np.imag(u),self.y,'g',np.sqrt(u*np.conjugate(u)),self.y,'m',lw=2)
-		ay2.set_ylabel(r'$y$',fontsize=32)
-		ay2.set_xlabel(r"$u^'$",fontsize=32)	
-		#lgd=ay2.legend((lines2),(r'$Re$',r'$Im$',r'$Mod$'),loc = 3,ncol=3, bbox_to_anchor = (0,1),fontsize=32)
-		ay2.set_ylim([0,5])
-		ay2.set_xlim([-1, 1])
-		ay2.grid()  
-
-
-		lines3 = ay3.plot(np.real(v),self.y,'r',np.imag(v),self.y,'g',np.sqrt(v*np.conjugate(v)),self.y,'m',lw=2)
-		ay3.set_ylabel(r'$y$',fontsize=32)
-		ay3.set_xlabel(r"$v^'$",fontsize=32)	
-		#lgd=ay3.legend((lines3),(r'$Re$',r'$Im$',r'$Mod$'),loc = 3,ncol=3, bbox_to_anchor = (0,1),fontsize=32)
-		ay3.set_ylim([0,5])
-		ay3.set_xlim([-1, 1])
-		ay3.grid()  
-
-		fig2.savefig('RESULTS'+'spfunrum_bla.png', bbox_inches='tight',dpi=50)     
-
-		plt.show(lines)	
-
-
-
+			p=eigfun_picked[0:self.N]
+			u=eigfun_picked[self.N:2*self.N]
+			v=eigfun_picked[2*self.N:3*self.N]
 
 		
-cc=fluid(-1,1,200)
+			fig2, (ay1,ay2,ay3) = plt.subplots(1,3)#, dpi=50)
+			lines1 = ay1.plot(np.real(p),self.y,'r',np.imag(p),self.y,'g',np.sqrt(p*np.conjugate(p)),self.y,'m',lw=2)
+			ay1.set_ylabel(r'$y$',fontsize=32)
+			ay1.set_xlabel(r"$p^'$",fontsize=32)	
+			lgd=ay1.legend((lines1),(r'$Re$',r'$Im$',r'$Mod$'),loc = 3,ncol=3, bbox_to_anchor = (0,1),fontsize=32)
+			ay1.set_ylim([0,5])
+			#ay1.set_xlim([-1, 1])
+			ay1.grid()                                         
+		
+			lines2 = ay2.plot(np.real(u),self.y,'r',np.imag(u),self.y,'g',np.sqrt(u*np.conjugate(u)),self.y,'m',lw=2)
+			ay2.set_ylabel(r'$y$',fontsize=32)
+			ay2.set_xlabel(r"$u^'$",fontsize=32)	
+			#lgd=ay2.legend((lines2),(r'$Re$',r'$Im$',r'$Mod$'),loc = 3,ncol=3, bbox_to_anchor = (0,1),fontsize=32)
+			ay2.set_ylim([0,5])
+			#ay2.set_xlim([-1, 1])
+			ay2.grid()  
+
+
+			lines3 = ay3.plot(np.real(v),self.y,'r',np.imag(v),self.y,'g',np.sqrt(v*np.conjugate(v)),self.y,'m',lw=2)
+			ay3.set_ylabel(r'$y$',fontsize=32)
+			ay3.set_xlabel(r"$v^'$",fontsize=32)	
+			#lgd=ay3.legend((lines3),(r'$Re$',r'$Im$',r'$Mod$'),loc = 3,ncol=3, bbox_to_anchor = (0,1),fontsize=32)
+			ay3.set_ylim([0,5])
+			#ay3.set_xlim([-1, 1])
+			ay3.grid()  
+
+			fig2.savefig('RESULTS'+'spfunrum_bla.png', bbox_inches='tight',dpi=50)     
+
+			plt.show(lines)	
 
 
 
 
-cc.set_perturbation(0.6,360)
+		
+cc=fluid(-1,1,400)
+
+
+
+
+cc.set_perturbation(0.4,160)
 cc.diff_matrix()
 #cc.set_poiseuille()
-cc.read_velocity_profile('DATA/H.txt')
+cc.read_velocity_profile('DATA/G.txt')
 #cc.read_velocity_profile('DATA/blasius.txt')
 
 """
 cc.mapping(1000)
-cc.interpolate()
+#cc.interpolate()
+cc.set_blasisus(cc.y)
 cc.build_operator()
 cc.BC1()
 cc.solve_eig()
 cc.plot_spectrum()
 
 """
-cc.mapping(500)
+cc.mapping(1000)
 cc.interpolate()
+#cc.set_blasisus(cc.y)
 cc.plot_velocity()
 cc.LNS()
 cc.BC_LNS()
 cc.solve_eig()
 cc.plot_LNS_eigspectrum()
-
-
-#print len(cc.eigv)
-#import scipy.io
-
-#scipy.io.savemat('test.mat', dict(x=cc.D))
 
