corrected bug in the normalization of GU, GCD in p u v variables

main.py
@@ -4,13 +4,13 @@ import sapy.sensitivity as sn
 import pdb as pdb
 import numpy as np
 
-option = {'flow': 'DATA/H.txt',
+option = {'flow': 'DATA/G.txt',
           'n_points': 300,
           'lc': 0.16739,
           'Ymax': 1000,
           'yi': 5,
-          'alpha':  0.1,
-          'Re': 1e5,
+          'alpha': 0.66552,
+          'Re': 157.922677,
           'variables': 'p_u_v',
           'equation': 'LNS_CD',
           'mapping': ['semi_infinite_PB', [0, (46.7/13.8)]],
@@ -36,7 +36,7 @@ f.interpolate()
 f.set_operator_variables()
 
 f.solve_eig()
-f.adjoint_spectrum_v_eta('disc')
+f.adjoint_spectrum('disc')
 f.solve_eig_adj()
 
 f.save_sim('200_puv_disc')
@@ -46,12 +46,12 @@ f.save_sim('200_puv_disc')
 v = po.viz('200_puv_disc.npz')
 v.plot_velocity()
 v.plot_spectrum()
-#f.omega_alpha_curves(0.1,1,10)
+
+#f.omega_alpha_curves(0.1,1,30, 'G_RE_157')
 
 idx = np.argmax(np.imag(f.eigv))
 om = sn.sensitivity('200_puv_disc.npz', idx)
 a, b, c, d = om.c_per(obj='norm')
-
 print a, b, c,d
 
 #om.sens_spectrum('ke_cd_N001_puv.png', 1e-3, 1e-2, obj='u', shape='sin') # eps, gamma

sapy/modal.py
@@ -557,7 +557,7 @@ class fluid(object):
         self.daCD = np.concatenate([(np.ones(len(self.y) - len(y_int))) * 0,
                                    f_daCD(y_int)])
 
-    def adjoint_spectrum_v_eta(self, method):
+    def adjoint_spectrum(self, method):
         I = np.identity(self.N)
         i = (0+1j)
         delta = self.D[1] - self.alpha**2 * I
@@ -710,20 +710,23 @@ class fluid(object):
 
 
     @nb.jit
-    def omega_alpha_curves(self, alpha_start, alpha_end, n_step):
+    def omega_alpha_curves(self, alpha_start, alpha_end, n_step, name_file='omega_alpha'):
         self.vec_alpha = np.linspace(alpha_start, alpha_end, n_step)
         self.vec_eigv_im = np.zeros(n_step)
         for i in np.arange(n_step):
             self.set_perturbation(self.vec_alpha[i], self.Re)
-            self.choose_variables()
+            self.set_operator_variables()
             self.solve_eig()
             # self.vec_eigv_im[i] = np.max(self.eigv_im)
 
-            self.vec_eigv_im[i] = (self.vec_alpha[i] *
-                                   np.max(self.eigv_im[self.eigv_im < 1]))
+            self.vec_eigv_im[i] = (self.vec_alpha[i] * np.max(np.imag(self.eigv)))
 
             # print self.eigv_im
-        np.savez('euler_cd_turb', self.vec_alpha, self.vec_eigv_im)
+        np.savez('omega_alpha_'+name_file, self.vec_alpha, self.vec_eigv_im)
+
+        header = 'alpha  omega_i'
+        np.savetxt('omega_alpha_'+name_file+'.txt' ,np.transpose([self.vec_alpha, self.vec_eigv_im]), fmt='%.4e', delimiter=' ', newline='\n', header=header)
+
 
         fig, ay = plt.subplots(dpi=150)
         lines = ay.plot(self.vec_alpha, self.vec_eigv_im, 'b', lw=2)
@@ -735,7 +738,7 @@ class fluid(object):
         # ay.set_xlim([0, 1.8])
         ay.grid()
         # plt.tight_layout()
-        fig.savefig('euler_cd_turb.png', bbox_inches='tight', dpi=150)
+        fig.savefig('omega_alpha_'+name_file+'.png', bbox_inches='tight', dpi=150)
         plt.show(lines)
 
     def set_perturbation(self, a, Re):
@@ -811,7 +814,7 @@ class fluid(object):
                  ddU=self.ddU, aCD=self.aCD, daCD=self.daCD,
                  eigv=self.eigv, eigf=self.eigf, D=self.D,
                  adj_eigv=self.eigv_adj, adj_eigf=self.eigf_adj,
-                 integ_matrix=self.integ_matrix, alpha=self.alpha)
+                 integ_matrix=self.integ_matrix, alpha=self.alpha, Re=self.Re, flow=self.option['flow'])
 
     def check_adj(self):
         H = (self.A - self.eigv[16]*self.B)

sapy/post.py
@@ -36,6 +36,8 @@ class viz(object):
     def __init__(self, in_data):
         # as input needs the in_data.npz with the simulation results
         data = np.load(in_data)
+        self.Re = data['Re']
+        self.flow = data['flow']
         self.y = data['y']
         self.U = data['U']
         self.dU = data['dU']
@@ -90,7 +92,7 @@ class viz(object):
             #np.savetxt(file_name, np.c_[self.eigv_re, self.eigv_im, self.eigv_re_adj, self.eigv_im_adj],
             #        fmt='%1.4e',  header='eigv_re    eigv_im    eigv_re_adj    eigv_im_adj')
             np.savetxt(file_name, np.c_[self.eigv_re*self.alpha, self.eigv_im*self.alpha, self.eigv_re_adj*self.alpha, self.eigv_im_adj*self.alpha],
-                            fmt='%1.4e',  header='ALPHA = 0.6 eigv_re    eigv_im    eigv_re_adj    eigv_im_adj')
+                            fmt='%1.4e',  header=str(self.option)+'\n'+'eigv_re    eigv_im    eigv_re_adj    eigv_im_adj')
 
             #  for i in np.arange(10):
             self.fig, ay = plt.subplots(figsize=(10, 6), dpi=100)
@@ -314,7 +316,7 @@ class viz(object):
                 np.savetxt(file_name_fun+'.out', np.transpose([self.y, p_re, p_im, p_mod,
                 u_re, u_im, u_mod, v_re, v_im, v_mod, p_re_adj, p_im_adj, p_mod_adj,
                 u_re_adj, u_im_adj, u_mod_adj, v_re_adj, v_im_adj, v_mod_adj]), fmt='%1.4e',
-                header='y   p_re    p_im    p_mod   u_re    u_im    u_mod    v_re    v_im    v_mod \
+                header=str(self.option)+'\n'+'y   p_re    p_im    p_mod   u_re    u_im    u_mod    v_re    v_im    v_mod \
                     p_re_adj    p_im_adj    p_mod_adj   u_re_adj    u_im_adj    u_mod_adj    v_re_adj    v_im_adj    v_mod_adj')
 
                 plt.show()

sapy/sensitivity.py
@@ -44,6 +44,7 @@ class sensitivity(object):
 
         # as input needs the in_data.npz with the simulation results
         data = np.load(in_data)
+        self.Re = data['Re']
         self.y = data['y']
         self.U = data['U']
         self.dU = data['dU']
@@ -159,7 +160,7 @@ class sensitivity(object):
         ay.grid()
         plt.show(lines)
 
-    def c_per(self, obj='u', *args):
+    def c_per(self, obj='u', file_name='sens_fun.out', *args):
         i = (0 + 1j)
         if self.option['variables'] == 'v_eta':
             v = self.eigf[:, self.idx]
@@ -210,7 +211,7 @@ class sensitivity(object):
             v_adj_conj = np.conjugate(v_adj)
             u_adj_conj = np.conjugate(u_adj)
 
-            f_norm = v_adj_conj*v + u*u_adj_conj*p
+            f_norm = v_adj_conj*v + u*u_adj_conj
             normaliz = np.sum(self.integ_matrix*f_norm)
 
             I = np.eye(len(v))
@@ -223,6 +224,9 @@ class sensitivity(object):
                     +v*v_adj_conj +u*u_adj_conj)/normaliz
 
             Gcd = ((-(i*0.552)/self.alpha)*self.U*u*u_adj_conj)/normaliz
+
+            np.savetxt(file_name, np.c_[self.y, np.abs(Gu), np.abs(Gcd), np.real(Gu), np.imag(Gu), np.real(Gcd), np.imag(Gcd)],
+                            fmt='%1.4e',  header=str(self.option)+'\n'+'\n'+'MAX |Gu|:'+str(np.max(np.abs(Gu)))+'MAX |Gcd|:'+str(np.max(np.abs(Gcd)))+'\n'+'y   |Gu|    |Gcd|    Gu_real     Gu_imag     Gcd_real    Gcd_imag')
 
 
         phase_Gu = np.arctan(np.imag(Gu)/np.real(Gu))

spectrum_alpha.py
@@ -32,7 +32,7 @@ f.superpose_spectrum(0.1, 1, 10)
 
 """
 
-a = np.linspace(0.1, 0.6, 15)
+a = np.linspace(0.4, 0.7, 10)
 #c = np.arange(0.1, 1.1, 0.02)
 #b = np.arange(1.1, 4.1, 0.1)
 #a = np.concatenate((c,b))
@@ -46,13 +46,13 @@ norm_gcdIm = np.zeros(len(a))
 
 for i in np.arange(len(a)):
 
-    option = {'flow': 'DATA/H.txt',
+    option = {'flow': 'DATA/G.txt',
               'n_points': 300,
               'lc': 0.16739,
               'Ymax': 1000,
               'yi': 10,
               'alpha': a[i],
-              'Re': 1e5,
+              'Re': 157.922677,
               'variables': 'p_u_v',
               'equation': 'LNS_CD',
               'mapping': ['semi_infinite_PB', [0, (46.7/13.8)]],
@@ -72,13 +72,13 @@ for i in np.arange(len(a)):
     #print idx
     eigv_sel[i] = f.eigv[idx]
 
-    f.adjoint_spectrum_v_eta('disc')
+    f.adjoint_spectrum('disc')
     f.solve_eig_adj()
     f.save_sim('200_puv_disc')
     om = sn.sensitivity('200_puv_disc.npz', idx)
     norm_guRe[i], norm_guIm[i], norm_gcdRe[i], norm_gcdIm[i] = om.c_per(obj='norm')
 
-np.savez('norm_alpha_H_300', alpha=a, norm_guRe=norm_guRe, norm_guIm=norm_guIm, norm_gcdRe=norm_gcdRe, norm_gcdIm=norm_gcdIm)
+np.savez('norm_alpha_G_300_RE_157', alpha=a, norm_guRe=norm_guRe, norm_guIm=norm_guIm, norm_gcdRe=norm_gcdRe, norm_gcdIm=norm_gcdIm)
 
 
 fig, ay  =  plt.subplots(dpi = 100)